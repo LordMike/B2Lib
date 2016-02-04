31b4aeb7fb8a4136e11ffed434c8b35b5d951f17
Michael Bisbjerg <michael@mbwarez.dk>
2016-02-04 18:37:00 +0100

    Cmdlet missing base class

--- B2Powershell/RemoveFilesCommand.cs
+++ B2Powershell/RemoveFilesCommand.cs
@@ -1,29 +1,20 @@
 using System.Management.Automation;
-using B2Lib;
 using B2Lib.Objects;
 using B2Lib.SyncExtensions;
 
 namespace B2Powershell
 {
     [Cmdlet(VerbsCommon.Remove, "B2Files")]
-    public class RemoveFilesCommand : PSCmdlet
+    public class RemoveFilesCommand : B2CommandWithSaveState
     {
-        [Parameter(Mandatory = true, Position = 0)]
-        public B2SaveState State { get; set; }
-        
         [Parameter(Mandatory = true, ValueFromPipeline = true, Position = 1)]
         public B2FileBase[] Files { get; set; }
 
-        protected override void ProcessRecord()
+        protected override void ProcessRecordInternal()
         {
-            base.ProcessRecord();
-
-            B2Client client = new B2Client();
-            client.LoadState(State);
-
             foreach (B2FileBase file in Files)
             {
-                bool result = client.DeleteFile(file);
+                bool result = Client.DeleteFile(file);
                 WriteObject(result);
             }
         }
