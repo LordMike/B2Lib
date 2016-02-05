a8a9dab5800c3c61178448cbadb02faf857abc09
Michael Bisbjerg <michael@mbwarez.dk>
2016-02-05 20:39:33 +0100

    Fix bug in Export-State

--- B2Powershell/ExportStateCommand.cs
+++ B2Powershell/ExportStateCommand.cs
@@ -5,7 +5,7 @@ namespace B2Powershell
     [Cmdlet("Export", "B2State")]
     public class ExportStateCommand : B2CommandWithSaveState
     {
-        [Parameter(Mandatory = true)]
+        [Parameter(Mandatory = true, Position = 0)]
         public string File { get; set; }
 
         protected override void ProcessRecordInternal()
